14202c93bf6da648f60a3e324360fa83bf5e1447
Caleb Anderson <robotrising@gmail.com>
2015-06-08 11:26:37 -0600

    Support driving strips from the center. Remove strobe.

--- KiteLight.ino
+++ KiteLight.ino
@@ -1,25 +1,28 @@
 #include <FastLED.h>
 
-#define LED_PIN     6
-#define NUM_LEDS    120
-#define BRIGHTNESS  255
-#define LED_TYPE    WS2812B
-#define COLOR_ORDER GRB
+#define LED_STRIP_1_PIN      5
+#define LED_STRIP_2_PIN      6
+#define NUM_LEDS_PER_STRIP   60
+#define NUM_STRIPS           2
+#define NUM_LEDS             (NUM_LEDS_PER_STRIP * NUM_STRIPS)
+
+#define USABLE_WIDTH         (NUM_LEDS - (BUMPER_WIDTH * 2))
+#define USABLE_HALF          (USABLE_START + USABLE_WIDTH / 2)
+#define USABLE_START         (BUMPER_WIDTH)
+
+#define BRIGHTNESS           255
+#define LED_TYPE             WS2812B
+#define COLOR_ORDER          GRB
 
 #define CYLON_EYE_HALF_WIDTH 10
-#define BUMPER_WIDTH 4
+#define BUMPER_WIDTH         5
 
-#define USABLE_WIDTH (NUM_LEDS - (BUMPER_WIDTH * 2))
-#define USABLE_HALF (USABLE_START + USABLE_WIDTH / 2)
-#define USABLE_START (BUMPER_WIDTH)
+#define CYLON_DELAY          25
+#define POLICE_DELAY         150
+#define ALTERNATING_DELAY    5
+#define PLASMA_DELAY         50
 
-#define CYLON_DELAY  25
-#define POLICE_DELAY 150
-#define ALTERNATING_DELAY 5
-#define STROBE_DELAY 50
-
-#define RUN_TIME 20000 /* 20 seconds */
-#define STROBE_RUN_TIME 7500
+#define RUN_TIME             20000 /* 20 seconds */
 
 CRGB leds[NUM_LEDS];
 
@@ -27,21 +30,45 @@ int redLookup[CYLON_EYE_HALF_WIDTH];
 
 void setup() {
   delay(3000); // power-up safety delay
-  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+
+  FastLED.addLeds<LED_TYPE, LED_STRIP_1_PIN, COLOR_ORDER>(leds, 0, NUM_LEDS_PER_STRIP).setCorrection(TypicalLEDStrip);
+  FastLED.addLeds<LED_TYPE, LED_STRIP_2_PIN, COLOR_ORDER>(leds, NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP).setCorrection(TypicalLEDStrip);
+
   FastLED.setBrightness(BRIGHTNESS);
   FastLED.setDither(1);
 
   generate_cylon_lookup_values();
 }
 
+uint16_t fix_index(uint16_t index) {
+  if (index < NUM_LEDS_PER_STRIP) {
+    return NUM_LEDS_PER_STRIP - index - 1;
+  }
+
+  // else
+  return index;
+}
+
 void loop()
 {
   plasma(RUN_TIME);
-  strobe(STROBE_RUN_TIME);
   rainbow(RUN_TIME);
   police_lights(RUN_TIME);
   fire_random_alternating(RUN_TIME);
   cylon(RUN_TIME);
+}
+
+void bumpers() {
+  for (int i = 0; i < BUMPER_WIDTH - 1; i++) {
+    leds[fix_index(i)].setColorCode(CRGB::Yellow);
+    leds[fix_index(NUM_LEDS - 1 - i)].setColorCode(CRGB::Aqua);
+  }
+
+  leds[fix_index(NUM_LEDS - BUMPER_WIDTH)].setRGB(0, 0, 0);
+  leds[fix_index(BUMPER_WIDTH - 2)].setRGB(0, 0, 0);
+
+  leds[fix_index(NUM_LEDS - BUMPER_WIDTH + 1)].setRGB(0, 0, 0);
+  leds[fix_index(BUMPER_WIDTH - 1)].setRGB(0, 0, 0);
 }
 
 void plasma(uint32_t time) {
@@ -50,31 +77,14 @@ void plasma(uint32_t time) {
 
   while (millis() - now < time) {
     for (uint32_t i = 0; i < USABLE_WIDTH; ++i) {
-      // int8_t sin = cubicwave8(n + i * 12);
       int8_t wave = cubicwave8(i + sin8(n + i) + cos8(n - i));
-      leds[i + USABLE_START] = HeatColor(wave);
+      leds[fix_index(i + USABLE_START)] = HeatColor(wave);
     }
     n++;
 
     bumpers();
     FastLED.show();
-    FastLED.delay(STROBE_DELAY);
-  }
-}
-
-void strobe(uint32_t time) {
-  unsigned long now = millis();
-
-  while (millis() - now < time) {
-    fill_solid(&(leds[USABLE_START]), USABLE_WIDTH, CRGB::White);
-    bumpers();
-    FastLED.show();
-    FastLED.delay(STROBE_DELAY);
-
-    fill_solid(&(leds[USABLE_START]), USABLE_WIDTH, CRGB::Black);
-    bumpers();
-    FastLED.show();
-    FastLED.delay(STROBE_DELAY);
+    FastLED.delay(PLASMA_DELAY);
   }
 }
 
@@ -100,10 +110,10 @@ void police_lights(uint32_t time) {
     for (int n = USABLE_START; n < USABLE_WIDTH + USABLE_START; ++n )
     {
       if (n < USABLE_HALF) {
-        leds[n].setColorCode(blue);
+        leds[fix_index(n)].setColorCode(blue);
       }
       else if (n >= USABLE_HALF) {
-        leds[n].setColorCode(red);
+        leds[fix_index(n)].setColorCode(red);
       }
     }
     bumpers();
@@ -113,10 +123,10 @@ void police_lights(uint32_t time) {
     for (int n = USABLE_START; n < USABLE_WIDTH + USABLE_START; ++n )
     {
       if (n < USABLE_HALF) {
-        leds[n].setColorCode(red);
+        leds[fix_index(n)].setColorCode(red);
       }
       else
-        leds[n].setColorCode(blue);
+        leds[fix_index(n)].setColorCode(blue);
     }
     bumpers();
     FastLED.show();
@@ -133,10 +143,10 @@ void fire_random_alternating(uint32_t time) {
     int b = random8();
     int start = USABLE_HALF;
     for (int n = 0; n < start; ++n) {
-      leds[start + 1 * n].setRGB(r, g, b);
-      leds[start - 1 * n].setRGB(r, g, b);
-      leds[start + 1 * (n - 1)].setRGB(0, 0, 0);
-      leds[start - 1 * (n - 1)].setRGB(0, 0, 0);
+      leds[fix_index(start + 1 * n)].setRGB(r, g, b);
+      leds[fix_index(start - 1 * n)].setRGB(r, g, b);
+      leds[fix_index(start + 1 * (n - 1))].setRGB(0, 0, 0);
+      leds[fix_index(start - 1 * (n - 1))].setRGB(0, 0, 0);
       bumpers();
       FastLED.show();
       FastLED.delay(ALTERNATING_DELAY);
@@ -155,23 +165,13 @@ void generate_cylon_lookup_values() {
   }
 }
 
-void bumpers() {
-  for (int i = 0; i < BUMPER_WIDTH - 1; i++) {
-    leds[NUM_LEDS - 1 - i].setRGB(255, 255, 0);
-    leds[i].setRGB(0, 255, 255);
-  }
-
-  leds[NUM_LEDS - BUMPER_WIDTH].setRGB(0, 0, 0);
-  leds[BUMPER_WIDTH - 1].setRGB(0, 0, 0);
-}
-
 void cylon_eye_piece(int i) {
   for (int n = 0; n < CYLON_EYE_HALF_WIDTH; ++n) {
     if ((i + n) < USABLE_WIDTH + USABLE_START) {
-      leds[i + n].setRGB(redLookup[n], 0, 0);
+      leds[fix_index(i + n)].setRGB(redLookup[n], 0, 0);
     }
     if ((i - n) >= USABLE_START) {
-      leds[i - n].setRGB(redLookup[n], 0, 0);
+      leds[fix_index(i - n)].setRGB(redLookup[n], 0, 0);
     }
   }
 }
